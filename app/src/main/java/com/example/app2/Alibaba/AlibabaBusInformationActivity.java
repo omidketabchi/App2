c0c3bbbd3e61899f255758fa95df72deb943058b
Omid Ketabchi <ketabchi.omid@hotmail.com>
2020-02-08 17:44:31 +0330

    initial commit-4

--- app/src/main/java/com/example/app2/Alibaba/AlibabaBusInformationActivity.java
+++ app/src/main/java/com/example/app2/Alibaba/AlibabaBusInformationActivity.java
@@ -37,7 +37,7 @@ public class AlibabaBusInformationActivity extends AppCompatActivity {
     RecyclerView recyclerView;
 
     List<AlibabaChairModel> chairModels;
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
